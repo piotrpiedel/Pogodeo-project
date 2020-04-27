5de32fbb3d030fa16273ad6828d2672eaec1c73b
piotrpiedel <piotrpiedel96@gmail.com>
2020-04-27 16:31:39 +0200

    Remove history mode, remove useless components
    Reformat code
    Remove findLocation
    Remove helloWorld
    Remove history mode for routing

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -4,52 +4,40 @@ import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: () =>
+            import ("../views/About.vue"),
+    },
 
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-
-  {
-    path: "/hello-world",
-    name: "HelloWorld",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/storm",
-    name: "Strom",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Storm.vue"),
-  },
-  {
-    path: "/fivedays",
-    name: "FiveDays",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/FiveDays.vue"),
-  },
-  {
-    path: "/detailed",
-    name: "Detailed",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Detailed.vue"),
-  },
+    {
+        path: "/storm",
+        name: "Strom",
+        component: () =>
+            import ("../views/Storm.vue"),
+    },
+    {
+        path: "/fivedays",
+        name: "FiveDays",
+        component: () =>
+            import ("../views/FiveDays.vue"),
+    },
+    {
+        path: "/detailed",
+        name: "Detailed",
+        component: () =>
+            import ("../views/Detailed.vue"),
+    },
 ];
 
 const router = new VueRouter({
-  routes,
-  mode: "history",
+    routes,
 });
 
 export default router;
